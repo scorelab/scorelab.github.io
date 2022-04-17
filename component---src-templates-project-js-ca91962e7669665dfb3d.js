bbda84be41c01fac3ca0ec06075a7541d1edd579
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-04-17 19:49:49 +0530

    Updates

--- component---src-templates-project-js-ca91962e7669665dfb3d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{GhMY:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return i})),n.d(e,"postQuery",(function(){return l}));var r=n("q1tI"),a=n.n(r),o=n("jfxn"),c=n("wtQ5"),u=n("8bOL");function i(t){var e=t.data.markdownRemark.frontmatter;return a.a.createElement(o.a,null,a.a.createElement(c.a,{title:"Score Labs "+e.title+" Project"}),a.a.createElement(u.o,{project:e}))}var l="1233456436"}}]);
-//# sourceMappingURL=component---src-templates-project-js-ca91962e7669665dfb3d.js.map
